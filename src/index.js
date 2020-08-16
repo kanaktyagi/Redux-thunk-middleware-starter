205cc469e5c0c264ae362476a92db3aa0c84358d
Kanak Tyagi <tyagiknk.india@gmail.com>
2020-08-16 15:03:31 +0530

    Applied the midddleware in the index.js file

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {Provider} from 'react-redux'
-import {createStore} from 'redux'
-import reducer from './store/reducer'
+import {createStore, applyMiddleware} from 'redux'
+import reducer from './store/reducers/reducer'
+import thunk from 'redux-thunk'
 
 
-const store = createStore(reducer)
+const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   

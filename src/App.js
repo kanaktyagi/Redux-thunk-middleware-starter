1e401bcfb6b4bc696b60e4b6df518702fe6dcc79
Kanak Tyagi <tyagiknk.india@gmail.com>
2020-08-16 15:08:14 +0530

    Adds function call from action creator

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React, {Component} from 'react';
 import './App.css';
 import {connect} from 'react-redux'
+import * as actionCreator from './store/actions/actions'
 
 class App extends Component { 
   render() {
     return(
       <div className="App">
       <div className="age "><span>Age:{this.props.age}</span></div> 
+      <br/>
       <button onClick={this.props.onAgeUp}> Age Up:</button>
       <button onClick={this.props.onAgeDown}> Age Down:</button>
+      <br/>
+      <hr/>
       <div> History:</div>
       <ul>
       {
         this.props.history.map((his) => {
-         return( <li key={his.id} onClick={() => this.props.onDelItem(his.id)}>{his.age}</li>)
+         return( <li><button key={his.id} onClick={() => this.props.onDelItem(his.id)}>{his.age}</button></li>)
         })
       }
       </ul>
@@ -33,9 +37,9 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps =(dispatch) => {
   return {
-    onAgeUp: () => dispatch({type:'AGE_UP', value: 1}),
-    onAgeDown: () => dispatch({type:'AGE_DOWN', value:1 }),
-    onDelItem: (id) => dispatch({type:'DEL_ITEM', key: id})
+    onAgeUp: () => dispatch(actionCreator.ageUp(1)),
+    onAgeDown: () => dispatch(actionCreator.ageDown(1)),
+    onDelItem: (id) => dispatch(actionCreator.delItem(id))
   }
 }
 export default connect(mapStateToProps, mapDispatchToProps) (App); 
